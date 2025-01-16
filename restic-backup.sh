fbf8ec7045ac36d0b592956fe73c3401c51e5379
Christian Drumm <christian.drumm@hey.com>
2025-01-16 07:30:49 +0100

    Changed retention period of snapshots to 12 month

--- restic-backup.sh
+++ restic-backup.sh
@@ -3,27 +3,27 @@
 PID_FILE=~/.config/restic/.restic_backup.pid
 CONFIG_FILE=~/.config/restic/restic.conf
 
-
 if [[ -f "$CONFIG_FILE" ]]; then
-  source $CONFIG_FILE
+	source $CONFIG_FILE
 else
-  echo $(date +"%Y-%m-%d %T") "File $CONFIG_FILE does not exist. Please create the required config file."
-  exit 1
+	echo $(date +"%Y-%m-%d %T") "File $CONFIG_FILE does not exist. Please create the required config file."
+	exit 1
 fi
 
 if [[ -f "$PID_FILE" ]]; then
-  if ps -p $(cat $PID_FILE) > /dev/null; then
-    echo $(date +"%Y-%m-%d %T") "File $PID_FILE exist. Probably backup is already in progress."
-    exit 2
-  else
-    echo $(date +"%Y-%m-%d %T") "File $PID_FILE exist but process " $(cat $PID_FILE) " not found. Removing PID file."
-    rm $PID_FILE
-  fi
+	if ps -p $(cat $PID_FILE) >/dev/null; then
+		echo $(date +"%Y-%m-%d %T") "File $PID_FILE exist. Probably backup is already in progress."
+		exit 2
+	else
+		echo $(date +"%Y-%m-%d %T") "File $PID_FILE exist but process " $(cat $PID_FILE) " not found. Removing PID file."
+		rm $PID_FILE
+	fi
 fi
 
-if [[ $(networksetup -getairportnetwork en0 | grep -E "Mat-Mina-Rus-Ada|other-networks") == "" ]]; then
-  echo $(date +"%Y-%m-%d %T") "Unsupported network."
-  exit 3
+if [[ $(ipconfig getsummary en0 | grep -E "Mat-Mina-Rus-Ada|other-networks") == "" ]]; then
+	echo $(date +"%Y-%m-%d %T") "Unsupported network."
+	osascript -e 'display notification "Restic backup not started." with title "Restic" subtitle "Not connected to home network."'
+	exit 3
 fi
 
 #if [[ $(pmset -g ps | head -1) =~ "Battery" ]]; then
@@ -32,7 +32,7 @@ fi
 #fi
 
 # Backup
-echo $$ > $PID_FILE
+echo $$ >$PID_FILE
 echo $(date +"%Y-%m-%d %T") "Backup start"
 
 export RESTIC_REPOSITORY
@@ -41,13 +41,14 @@ export RESTIC_PASSWORD
 # sometimes there are stale locks
 # remove them to be sure the backup runs
 /opt/homebrew/bin/restic unlock
-/opt/homebrew/bin/restic backup --verbose $BACKUP_PATHS --exclude-file=$EXCLUDE_FILE 
+/opt/homebrew/bin/restic backup --verbose $BACKUP_PATHS --exclude-file=$EXCLUDE_FILE
 
-if [[ $0 = 0 ]]; then
-  echo $(date +"%Y-%m-%d %T") "Backup finished successfully."
+if [[ $? = 0 ]]; then
+	echo $(date +"%Y-%m-%d %T") "Backup finished successfully."
+	osascript -e 'display notification "Restic backup finished successfully." with title "Restic" subtitle "Backup successfull."'
 else
-  echo $(date +"%Y-%m-%d %T") "Backup not sucessfull."
-  osascript -e 'display notification "Restic: Backup not successfull! Check log for details."'   
+	echo $(date +"%Y-%m-%d %T") "Backup not sucessfull."
+	osascript -e 'display notification "Restic backup not successfull! Check log for details." with title "Restic" subtitle "Backup failed."'
 fi
 # Housekeeping
 echo $(date +"%Y-%m-%d %T") "Housekeeping start"
